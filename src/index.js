edc6643b317a3f486e48188dae286a8458aa2bf5
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-11 16:17:21 +0530

    More starting work on the backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("API Backend");
+const express = require("express");
+// const morgan = require("morgan");
+
+require("dotenv").config();
+require("colors");
+
+const app = express();
+const { NODE_ENV, PORT } = process.env;
+const port = Number(PORT);
+
+app.get("/", (_req, res) => res.send("Hello"));
+app.listen(port, async function bootApp() {
+  console.log(
+    `Server started on port: ${port} and in ${NODE_ENV} mode`.yellow.bold,
+  );
+});
