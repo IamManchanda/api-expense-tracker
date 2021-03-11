fce7c626b971ac439dcc84ef02bd85bdb453b222
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-11 20:00:42 +0530

    Backend Deployment

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 require("colors");
 
 const app = express();
-const { NODE_ENV, PORT } = process.env;
+const { NODE_ENV, PORT } = process.env || 5000;
 const port = Number(PORT);
 
-app.get("/", (_req, res) => res.send("Hello"));
+app.get("/", (_req, res) => res.send("Hello!"));
 app.listen(port, async function bootApp() {
   console.log(
     `Server started on port: ${port} and in ${NODE_ENV} mode`.yellow.bold,
